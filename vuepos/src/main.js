1da9eba92152398cac644a785333ed709c1320f7
FrankMa <semstar2010@gmail.com>
2018-08-12 11:43:20 +1200

    vue mock request

--- vuepos/src/main.js
+++ vuepos/src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-
-Vue.use(VueRouter)
-Vue.config.productionTip = false
-
 import Pos from './components/Pos.vue'
 import Products from './components/Products.vue'
 import ProductDetail from './components/ProductDetail.vue'
+import './request'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false
 
 const router = new VueRouter({
   routes: [
